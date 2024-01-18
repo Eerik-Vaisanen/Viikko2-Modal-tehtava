41697f20e5617bc0842fdf8c77d4824767f51c85
Eerik <112471297+Eerik-Vaisanen@users.noreply.github.com>
2024-01-18 02:39:07 +0200

    Update app.js
    Assigment done

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, Modal } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Open modal"
+        onPress={() => setModalVisible(true)}
+        color="midnightblue"
+      />
+      <Modal
+        animationType='fade'
+        onRequestClose={() => setModalVisible(false)}
+        visible={modalVisible}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.box}>
+            <Text>Modal content</Text>
+            <Button
+              title="Close"
+              color="midnightblue"
+              onPress={() => setModalVisible(false)}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -13,8 +35,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: 'lightgrey',
+    padding: 30,
+    borderRadius: 10,
+    elevation: 5, // Add elevation for a shadow effect (Android)
+    
   },
 });
